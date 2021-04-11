aee73ae0fafb0caf5e15133f1804ac8d7957ca67
merlynnvp <72741790+merlynnvp@users.noreply.github.com>
2021-04-11 16:29:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+//copy till//
+noseX=0;
+noseY=0;
+//here//
 function preload() 
 {
-
+//clown_nose = loadImage('')//
 }
 
 function setup()
@@ -25,14 +29,20 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = " + results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX)
+        console.log("nose y = " + noseY)
     }
 }
 
 function draw()
 {
 image(video, 0, 0, 300, 300);
+fill(255,0,0);
+stroke(255,0,0);
+circle(noseX, noseY, 20);
+//image(clown_nose, noseX, noseY, 30, 30);//
 }
 
 function take_snapshot()
